d458745d42893ddc7f28ec88c823941a10ce0dd1
wangzhihao <wzh07@hotmail.com>
2018-06-07 20:25:24 +0800

    [A]分割整数的最大乘积

---
+++ IntegerBreak.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class IntegerBreak {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= i - 1; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * dp[i - j], j * (i - j)));
+            }
+        }
+        return dp[n];
+    }
+    @Test
+    public void test(){
+        int n=20;
+        System.out.println(integerBreak(n));
+    }
+}
